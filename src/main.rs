0724728e832cca54060d13d35f00f65b803d0801
Sergey Bolshanin <sergey.bolshanin@mytona.com>
2025-03-02 14:35:41 +0700

    add title

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
+use std::time::Duration;
+
+use common::print_message;
 use docker_builder::process;
 
 mod common;
 mod finder;
 mod docker_builder;
 
+const TITLE :&str= r"
+////////////////////////////////////////////                                 
+///    _ _ __ ___ ____ _ __ _ ___ _ _    ///
+///   | '_/ _` \ V / _` / _` / -_) '_|   ///
+///   |_| \__,_|\_/\__,_\__, \___|_|     ///
+///                     |___/            ///
+////////////////////////////////////////////
+";
+
 
 fn main() {
+    print_message(TITLE,Duration::from_micros(1));
     process();
 }
