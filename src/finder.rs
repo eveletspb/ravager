af072692825ef52630b5c84b1210ef1a779ad270
eveletspb <dinospb@gmail.com>
2025-03-02 16:19:44 +0700

    rustfmt and update gitignore

--- src/finder.rs
+++ src/finder.rs
@@ -6,12 +6,11 @@ pub fn find(directory: &str, filter: &str) -> Vec<PathBuf> {
         for entry in entries {
             if let Ok(entry) = entry {
                 let path = entry.path();
-                if path.is_file() && path.file_name().unwrap() == filter  {
-                        vec.push(path.to_path_buf());
+                if path.is_file() && path.file_name().unwrap() == filter {
+                    vec.push(path.to_path_buf());
                 } else if path.is_dir() {
                     vec.extend(find(&path.to_str().unwrap(), filter));
                 }
-
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ use common::print_message;
 use docker_builder::process;
 
 mod common;
-mod finder;
 mod docker_builder;
+mod finder;
 
-const TITLE :&str= r"
+const TITLE: &str = r"
 ////////////////////////////////////////////                                 
 ///    _ _ __ ___ ____ _ __ _ ___ _ _    ///
 ///   | '_/ _` \ V / _` / _` / -_) '_|   ///
@@ -16,8 +16,7 @@ const TITLE :&str= r"
 ////////////////////////////////////////////
 ";
 
-
 fn main() {
-    print_message(TITLE,Duration::from_micros(1));
+    print_message(TITLE, Duration::from_micros(1));
     process();
 }
